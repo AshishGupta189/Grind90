8514124f142e4ea982a0c53a06e0308a8c9e9f47
Ashish Gupta <askd442@gmail.com>
2025-09-02 02:58:40 +0530

    learnt basics of socket-io(emit/on)

--- backend/chatbot/backend/server.js
+++ backend/chatbot/backend/server.js
@@ -1,16 +1,27 @@
+require("dotenv").config();
 const app = require("./src/app")
+
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const generateResponse = require("./src/service/ai.service")
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
-  // ...
+  console.log("Connection established");
+  socket.on("disconnect",()=>{
+    console.log("Connection destablished!..");
+  })
+
+  socket.on("message",async(data)=>{   //custom event
+    const response = await generateResponse(data.prompt);
+    console.log({"ai-response":response});
+    socket.emit("ai-response",{response});
+  })
 });
 
 
 httpServer.listen(3000,()=>{
     console.log("Server is running on port 3000");
-    
 })

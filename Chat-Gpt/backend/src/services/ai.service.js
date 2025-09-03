2424cd686a4d2ce6be289ffd20e9bd22c9d92fbd
Ashish Gupta <askd442@gmail.com>
2025-09-03 16:54:11 +0530

    added chatGpt day2

---
+++ Chat-Gpt/backend/src/services/ai.service.js
@@ -0,0 +1,13 @@
+const { GoogleGenAI } = require("@google/genai");
+
+const ai = new GoogleGenAI({});
+
+async function generateResponse(data) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: data,
+  });
+  return (response.text);
+}
+
+module.exports = {generateResponse};
